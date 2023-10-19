66cf435d9303d4497c6342c1878281cfb4e768bb
William Alef <robotalef@gmail.com>
2023-10-19 18:21:08 -0300

    Limpando o código

--- src/routes/postagem-route.js
+++ src/routes/postagem-route.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const postagemController = require('../controllers/postagem-controller');
 
-router.get('/get', postagemController.get); //Rota Get
-router.post('/post', postagemController.post); //Rota Post
-router.delete('/delete', postagemController.delete); //Rota Delete
-router.put('/update', postagemController.put); //Rota Update
+router.get('/get', postagemController.get);
+router.post('/post', postagemController.post);
+router.delete('/delete', postagemController.delete);
+router.put('/update', postagemController.put);
 
 module.exports = router;
 
