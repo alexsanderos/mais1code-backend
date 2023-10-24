7f370b9859abd5f9c4e3f9c34634673070a9feca
William Alef <robotalef@gmail.com>
2023-10-24 18:36:27 -0300

    Atualizando o código de erro.

--- src/controllers/contato-controller.js
+++ src/controllers/contato-controller.js
@@ -16,7 +16,7 @@ exports.post = async (req, res) => {
     try {
         const conteudo = req.body;
         const resultItem = await contatoRepository.criarContato(conteudo.id, conteudo.nome, conteudo.email, conteudo.cidade, conteudo.estado, conteudo.telefone, conteudo.assunto, conteudo.mensagem);
-        res.status(200).send({ message: 'Contato criado com sucesso!.', data: resultItem});
+        res.status(201).send({ message: 'Contato criado com sucesso!.', data: resultItem});
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Ocorreu um erro ao criar o contato.'});
