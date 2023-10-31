2f57d270d30ecc6edc5158d6bf0d17d8dd761e61
Byleozovisk <136724865+Byleozovisk@users.noreply.github.com>
2023-10-31 20:42:02 -0300

    1

--- src/repositories/oportunidades-repository.js
+++ src/repositories/oportunidades-repository.js
@@ -9,7 +9,7 @@ exports.obterOportunidades = async() => {
     }
 };
 
-exports.criarOportunidade = async(id, nome, depoimento) => {
+exports.criarOportunidade = async(id, nome, empresa, nivel, faixa_salarial_de, faixa_salarial_ate, regime) => {
     try {
         return clientBase.query(`INSERT INTO oportunidades (id, nome, empresa, nivel, faixa_salarial_de, faixa_salarial_ate, regime) values ($1, $2, $3, $4, $6, $7)`, [id, nome, empresa, nivel, faixa_salarial_de, faixa_salarial_ate, regime]);
     } catch (error) {
@@ -24,13 +24,13 @@ exports.excluirOportunidade = async(id) => {
         return clientBase.query(`DELETE FROM oportunidades WHERE id = $1`, [id]);
     } catch (error) {
         console.error(error);
-        res.status(400).json({ error: 'Ocorreu um erro ao excluir o contato no DB.' });
+        res.status(400).json({ error: 'Ocorreu um erro ao excluir a oportunidade no DB.' });
     }
 };
 
-exports.atualizarOportunidade = async(depoimento, id) => {
+exports.atualizarOportunidade = async(id, nome, empresa, nivel, faixa_salarial_de, faixa_salarial_ate, regime) => {
     try {
-        return clientBase.query(`UPDATE oportunidades SET oportunidades = $1 WHERE id = $2`, [depoimento, id]);
+        return clientBase.query(`UPDATE oportunidades SET oportunidades = $1 WHERE id = $2`, [id, nome, empresa, nivel, faixa_salarial_de, faixa_salarial_ate, regime]);
     } catch (error) {
         console.error(error);
         res.status(400).json({ error: 'Ocorreu um erro ao atualizar a oportunidade no DB.' });
