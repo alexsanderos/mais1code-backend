0eec98262115d36889a4365ebd3fbff33289c30a
William Alef <69725099+William-Alef@users.noreply.github.com>
2023-11-07 13:36:43 -0300

    Create postagem-repository.js

---
+++ src/repositories/postagem-repository.js
@@ -0,0 +1,37 @@
+const clientBase = require('../db/db');
+
+exports.obterPostagens = async() => {
+    try {
+        return clientBase.query(`SELECT * FROM depoimentos ORDER BY id`);
+    }catch (error) {
+        console.error(error);
+        throw error;
+    }
+}; 
+
+exports.criarPostagem = async(nome, depoimento) => {
+    try {
+        return clientBase.query(`INSERT INTO depoimentos (nome, depoimento) values ($1, $2)`, [nome, depoimento]);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ error: 'Ocorreu um erro ao inserir o contato no DB.'});
+        throw error;
+    }
+};
+exports.excluirPostagem = async(id) => {
+    try {
+        return clientBase.query(`DELETE FROM depoimentos WHERE id = $1`, [id]);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ error: 'Ocorreu um erro ao excluir o contato no DB.'});
+    }
+};
+
+exports.atualizarPostagem = async(depoimento, id) => {
+    try {
+        return clientBase.query(`UPDATE depoimentos SET depoimento = $1 WHERE id = $2`, [depoimento, id]);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ error: 'Ocorreu um erro ao atualizar o contato no DB.'});
+    }
+};
